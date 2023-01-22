5f749008c5e1bd4af7530cd05ba275f81ec583ef
Joven Manikiza <58902170+JManikiza@users.noreply.github.com>
2023-01-22 00:11:39 +0000

    A start on official work page.
    Fit in some nice touches and animations.
    Still a many more weeks to polish it and get some valued opinions.

---
+++ Portfolio/script.js
@@ -0,0 +1,44 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            console.log("Intersecting...");
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            console.log("Intersecting2...");
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const secondHiddenElements = document.querySelectorAll('.hidden1');
+secondHiddenElements.forEach((fi) => observer1.observe(fi));
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            console.log("Intersecting...");
+            entry.target.classList.add('show1');
+        } else {
+            entry.target.classList.remove('show1');
+        }
+    });
+});
+
+const thirdHiddenElements = document.querySelectorAll('.hidden2');
+thirdHiddenElements.forEach((ed) => observer2.observe(ed));
