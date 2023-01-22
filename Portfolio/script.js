d8a944bfcee5944642f836b16cf7e2d434a32ac9
Joven Manikiza <58902170+JManikiza@users.noreply.github.com>
2023-01-22 13:40:29 +0000

    Tested and functional
    Changed some variables for a smoother experience. Also edited JS with smooth scroll for when nav items are clicked.
    
    Introduced a favicon and adjusted the title

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -34,11 +34,17 @@ const observer2 = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             console.log("Intersecting...");
             entry.target.classList.add('show1');
-        } else {
-            entry.target.classList.remove('show1');
         }
     });
 });
 
 const thirdHiddenElements = document.querySelectorAll('.hidden2');
 thirdHiddenElements.forEach((ed) => observer2.observe(ed));
+
+window.scrollBy({ 
+    top: 100, // could be negative value
+    left: 0, 
+    behavior: 'smooth' 
+  });
+
+  window.onunload = function(){ window.scrollTo(0,0); }
