9f1dd2ea440b7e819a313ac3431878456e8d752e
Joven Manikiza <58902170+JManikiza@users.noreply.github.com>
2023-01-29 14:17:10 +0000

    Rework of colour scheme and changeability. Worked on aside element.
    -Added root values for colours so I can change the website layout upon click.
    
    -condensed the Intersection Observer in JS to help run the site smoother.
    
    -Animations have sped up but not yet optimal.
    
    -Added aside element with animation to creep into view upon button press. Using the button to call the JS func then add/removes classes

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,45 +1,26 @@
+//OBSERVER
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
-            console.log("Intersecting...");
-            entry.target.classList.add('show');
+            if (entry.target.classList.contains("hidden")) {
+                entry.target.classList.add('show');
+            } else if (entry.target.classList.contains("hidden1")) {
+                entry.target.classList.add('show');
+            } else if (entry.target.classList.contains("hidden2")) {
+                entry.target.classList.add('show1');
+            }
         } else {
-            entry.target.classList.remove('show');
+            if (entry.target.classList.contains("hidden")) {
+                entry.target.classList.remove('show');
+            }
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll('.hidden, .hidden1, .hidden2');
 hiddenElements.forEach((el) => observer.observe(el));
-
-const observer1 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            console.log("Intersecting2...");
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
-
-const secondHiddenElements = document.querySelectorAll('.hidden1');
-secondHiddenElements.forEach((fi) => observer1.observe(fi));
-
-const observer2 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            console.log("Intersecting...");
-            entry.target.classList.add('show1');
-        }
-    });
-});
-
-const thirdHiddenElements = document.querySelectorAll('.hidden2');
-thirdHiddenElements.forEach((ed) => observer2.observe(ed));
 
 window.scrollBy({ 
     top: 100, // could be negative value
@@ -47,4 +28,29 @@ window.scrollBy({
     behavior: 'smooth' 
   });
 
+//SMOOTHER SCROLLING
   window.onunload = function(){ window.scrollTo(0,0); }
+
+//ANIMATION
+
+function asideAnim() {
+
+    if(document.querySelector('.aside-container').classList.contains('move-left')){
+
+     document.querySelector('.aside-container').classList.remove('move-left');
+     document.querySelector('.logo-container').classList.remove('rotate');
+     document.getElementById("button-transform").classList.remove('cross');
+     document.getElementById("button-transform").classList.add('aside-button');
+
+    } else {
+
+        document.querySelector('.aside-container').classList.add('move-left');
+        document.querySelector('.logo-container').classList.add('rotate');
+        document.getElementById("button-transform").classList.add('cross');
+        document.getElementById("button-transform").classList.remove('aside-button');
+    }
+
+
+
+
+  }
